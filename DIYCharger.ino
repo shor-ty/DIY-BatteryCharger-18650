ffe48f4f45f336bdc8a4d24ac434c6676909a0bc
Tobias Holzmann <Tobias.Holzmann@Holzmann-cfd.de>
2022-04-15 00:43:00 +0200

    Base Commit for Li-Ion Charger
    This is the initial commit for the Li-Ion Charger DIY project.
    It will provide the software for the automatic charging and discharging
    of Li-Ion 18650 batteries. The aim of the software is to analyze already
    used batteries in terms of the remaining capacity (mAh). The software
    is built for n-battery setup and is switching between a charging and
    discharging mode based on the current and voltage measured at the battery.
    
    For the schematic of the PCB and the functionality, you will find explanations
    in the source code as well as on my YouTube channel named HolzmannCFD and the
    tag DIY.
    
    Enjoy.

---
+++ DIYCharger.ino
@@ -0,0 +1,155 @@
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+
+  Description
+    An automatic charger/discharger software for n cells. The programming
+    is based on an Wemos D1 Mini which takes care of 20 batteries. The
+    aim of the program is to analyse the remaining capacity of a used 18650
+    battery while making n discharge cycles.
+
+    The charing and discharging data are tracked and stored for each battery
+    separately in the SPIFF memory. After the test is performed, the data
+    are sent to some remote compute which has a database included.
+
+    In order to be able to understand everything, you can checkout my videos
+    at YouTube (HolzmannCFD) and the Tags DIY.
+
+  Author
+    Tobias Holzmann
+
+  Date
+    April 2022
+
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include "battery.h"
+
+// * * * * * * * * * * * * * Global Variables  * * * * * * * * * * * * * * * //
+
+#define slots 1
+unsigned int nSampling = 20;
+float Rdiss = 5.41;
+
+
+// * * * * * * * * * * * * * * Start Function  * * * * * * * * * * * * * * * //
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(D1, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+// * * * * * * * * * * * * * * Loop Function * * * * * * * * * * * * * * * * //
+
+void loop()
+{
+  Serial << " START PROGRAMM " << endl;
+  digitalWrite(D1, HIGH);
+
+  // Create the battery objects
+  Battery* batteries[slots];
+
+  for (int id = 0; id < slots; id++)
+  {
+    Serial << " **** Create the battery object **** " << endl;
+    batteries[id] = new Battery(id, millis(), 3.3);
+  }
+
+  bool show = true;
+
+  // Own loop in order to not destroy the object
+  do
+  {
+    // Loop through all batteries
+    for (auto& battery : batteries)
+    {
+      // First check if the battery is tested already, if not proceed, otherwise
+      // we will do the analysis of the battery
+      if (battery->mode() != Battery::TESTED)
+      {
+        if (battery->mode() != Battery::DISCHARGE && show == true)
+        {
+          Serial << " ++ Voltage is       : " << _FLOAT(battery->U(),6) << "\n";
+          Serial << "    Battery slot mode: ";
+
+          if (battery->mode() == Battery::EMPTY) Serial << "EMPTY\n";
+          else if (battery->mode() == Battery::CHARGE) Serial << "CHARGE\n";
+          else if (battery->mode() == Battery::DISCHARGE) Serial << "DISCHARGE\n";
+          else if (battery->mode() == Battery::FIRST) Serial << "FIRST\n";
+          Serial << "    Digital Pin D1   : " << digitalRead(D1) << "\n";
+          Serial << "    Number of dischar: " << battery->nDischarges() << "\n";
+          Serial << ">>>>>>>" << endl;
+          show = false;
+        }
+        else
+        {
+          delay(2000);
+        }
+
+        // Check if new battery was inserted
+        if(battery->checkIfReplacedOrEmpty())
+        {
+          if (battery->mode() == Battery::FIRST)
+          {
+            Serial<< " +++ NEW BATTERY DETECTED - RESET +++ " << endl;
+            battery->setOffset(millis());
+            battery->setU();
+            battery->setMode(Battery::CHARGE);
+          }
+        }
+
+        // Only execute the rest, if a battery is found
+        if
+        (
+            (battery->mode() != Battery::EMPTY)
+         && (battery->mode() != Battery::FIRST)
+        )
+        {
+          // Update all data corresponding on the battery mode
+          battery->update();
+
+          if (battery->mode() == Battery::CHARGE)
+          {
+            if(!battery->charging())
+            {
+              battery->setOffset(millis());
+              battery->checkIfFullyTested();
+            }
+          }
+
+          if (battery->mode() == Battery::DISCHARGE)
+          {
+            if(!battery->discharging())
+            {
+              battery->incrementDischarges();
+              battery->setMode(Battery::CHARGE);
+              battery->reset();
+              battery->setOffset(millis());
+              show = true;
+            }
+          }
+        }
+      }
+      else
+      {
+        Serial<< "Finished ...";
+        delay(60000);
+      }
+    }
+
+    // Show that the chip is running by simply putting the LED on for 1s
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(2000);
+  }
+  while (true);
+
+  // Destroy all objects if we ever went to this one
+  for (int id = 0; id < slots; ++id)
+  {
+    delete batteries[id];
+  }
+}
---
+++ battery.h
@@ -0,0 +1,197 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+
+  Description
+    Foobar
+
+  Author
+    Tobias Holzmann
+  Date
+    April 2020
+
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef battery_h
+#define battery_h
+
+#include "Arduino.h"
+#include "Streaming.h"
+
+/*****************************************************************************/
+
+class Battery
+{
+public:
+
+      // The modes to distiguish between different states
+      enum mode { CHARGE, DISCHARGE, EMPTY, FIRST, TESTED};
+
+
+private:
+
+  // Private class data
+
+    // Status of program (charge / discharge or no battery inserted)
+    mode mode_;
+
+    // Number of discharges to be perfomed
+    unsigned int nTotalDischarges_{2};
+
+    // Battery id
+    int id_;
+
+    // Channel
+    int channel_;
+
+    // Oversampling
+    int overSampling_;
+
+    // n cyclics for discharging
+    int nCycles_;
+
+    // Old time stamp (ms)
+    unsigned int tOld_;
+
+    // Current time stamp (ms)
+    unsigned int t_;
+
+    // Offset of time (ms)
+    unsigned long tOffset_;
+    
+
+    // Variables for capacity analysis
+
+      // Resistance used (Ohm)
+      float R_;
+    
+      // Actual voltage (V)
+      float U_;
+    
+      // Actual current (mA)
+      float I_;
+    
+      // Actual dissipated power (mW)
+      float P_;
+    
+      // Total battery capacity (mAh)
+      float C_;
+    
+      // Total battery energy (mWh)
+      float e_;
+    
+      // Discharging time (s)
+      float tDischarge_;
+
+      // Discharge cyclics
+      unsigned int nDischarges_;
+
+
+    // Variables for charging analysis
+
+      // charging time (s)
+      float tCharge_;
+
+      // Charging voltage (V)
+      float UCharge_;
+
+      // Charging current (mA)
+      float ICharge_;
+
+      // Container that stores the last n voltage data
+      float UBat_[20];
+   
+
+public:
+
+  // Constructor
+  Battery();
+  
+  Battery
+  (
+    const int,
+    const unsigned long,
+    const float
+  );
+
+  // Destroctor
+  ~Battery();
+
+
+  // Public functions
+
+    
+    // Return battery id
+    inline const int id() { return id_; };
+
+    // Check if battery was replaced or empty
+    const bool checkIfReplacedOrEmpty();
+
+    // Set new offset
+    void setOffset(const unsigned long);
+
+    // Set voltage (V)
+    void setU(const float);
+
+    // Set actual voltage (V)
+    void setU();
+
+    // Set current (mA)
+    void setI(const float);
+
+    // Increment the number of discharges
+    void incrementDischarges();
+
+    // Return the number of discharges
+    const unsigned int nDischarges() const;
+
+    // Set the mode
+    void setMode(const mode m);
+
+    // Return the mode
+    const enum mode mode() const;
+
+    // Return the voltage (V)
+    inline const float U() { return U_; }
+
+    // Return the time needed for charging
+    inline const float tCharge() const { return tCharge_; }
+    
+    // Reset all data to zero
+    void reset();
+
+    // Update all data which are needed based on the mode() of 
+    // the battery (charging/discharging)
+    void update();
+
+    // Function that determines if we are still charging or not
+    const bool charging();
+
+    // Function that determines if we are still discharging or not
+    const bool discharging();
+
+    // Function that determines if the battery is fully tested
+    void checkIfFullyTested();
+
+
+private:
+
+    // Read the digital signal at A0, convert it to a voltage and return it
+    const float readU();
+
+    // Convert the digital to an analog value
+    // This function provides a mapping from an digital value to an analog value
+    // by using an linear interpolation approach
+    float DtoA
+    (
+      const unsigned int,
+      const int,
+      const int,
+      const float,
+      const float
+    );
+};
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#endif
+
+// ************************************************************************* //
